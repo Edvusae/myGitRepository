d14a5d842b136b2faf53044933e9ebcb1b67d5af
Edvusae <105985332+Edvusae@users.noreply.github.com>
2025-11-29 09:26:44 +0200

    feat: Storage for all the date we will use

---
+++ js/storage.js
@@ -0,0 +1,16 @@
+// storage.js
+import { state } from "./state.js";
+
+const STORAGE_KEY = "neonfinance-data";
+
+export function saveState() {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(state.transactions));
+}
+
+export function loadState() {
+  const data = localStorage.getItem(STORAGE_KEY);
+  if (data) {
+    state.transactions = JSON.parse(data);
+  }
+}
+--- IGNORE ---
