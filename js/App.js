a800e0f6aced2cca678fa4f16090a6ff0828d946
Edvusae <105985332+Edvusae@users.noreply.github.com>
2025-11-29 09:56:41 +0200

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,53 +1,12 @@
-import { loadState } from "./storage/storage.js";
-import { addTransaction, calculateTotals } from "./transactions.js";
-import { renderTransactions, updateDashboard } from "./ui/ui.js";
-import { renderDonutChart } from "./chart.js";
-import { state } from "./state/state.js";
+import { loadTransactions } from "./storage.js";
+import { renderTransactions, updateBalance } from "./ui.js";
+import { initTransactionForm } from "./transactions.js";
 
-const form = document.getElementById("transaction-form");
-const filterType = document.getElementById("filter-type");
-const filterCat = document.getElementById("filter-category");
-const searchInput = document.getElementById("search-input");
-
-// Load saved data
-loadState();
-renderTransactions();
-updateDashboard();
-
-const totals = calculateTotals();
-renderDonutChart(totals.income, totals.expense);
-
-// Add transaction
-form.addEventListener("submit", e => {
-  e.preventDefault();
-
-  const data = {
-    text: form.text.value,
-    amount: Number(form.amount.value),
-    category: form.category.value,
-    date: form.date.value,
-    type: form.type.value
-  };
-
-  addTransaction(data);
-  form.reset();
+window.addEventListener("DOMContentLoaded", () => {
+    const data = loadTransactions();
+    renderTransactions(data);
+    updateBalance(data);
+    initTransactionForm();
 });
 
-// Filters
-filterType.addEventListener("change", () => {
-  state.filters.type = filterType.value;
-  renderTransactions();
-});
-
-filterCat.addEventListener("change", () => {
-  state.filters.category = filterCat.value;
-  renderTransactions();
-});
-
-// Search
-searchInput.addEventListener("input", () => {
-  state.filters.search = searchInput.value;
-  renderTransactions();
-});
-
-// Initial rendering on script load
+// --- IGNORE ---
